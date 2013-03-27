b28c1eac2404540eb7a031cc86949c416fbd21ff
Phan Văn Khanh <vankhanh.k55bk@gmail.com>
2013-03-27 17:35:45 +0700

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
+describe "Authentication" do
 
 	subject { page }
 	describe "signin page" do
@@ -60,6 +60,19 @@ describe "AuthenticationPages" do
 	describe "authorization" do
 		describe "for non-signed-in users" do
 			let(:user) {FactoryGirl.create(:user)}
+
+		     describe "in the Microposts controller" do
+
+		        describe "submitting to the create action" do
+		          before { post microposts_path }
+		          specify { response.should redirect_to(signin_path) }
+		        end
+
+		        describe "submitting to the destroy action" do
+		          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+		          specify { response.should redirect_to(signin_path) }
+		        end
+		      end
 
 			describe "in the Users controller" do
 				describe "visiting the user index" do
