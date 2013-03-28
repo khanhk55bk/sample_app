33e45e19e363bda7866b543943a86d41dbce6bc7
Phan Văn Khanh <vankhanh.k55bk@gmail.com>
2013-03-28 22:02:31 +0700

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,13 +13,48 @@ require 'spec_helper'
 
 describe User do
 
+
+
 	it { should respond_to(:password_confirmation)}
 	it { should respond_to(:remember_token)}
 	it { should respond_to(:authenticate)}
-	it {should respond_to(:admin)}
+	it { should respond_to(:admin)}
+
+	it { should respond_to(:feed)}
+	it { should respond_to(:relationships)}
+	it { should respond_to(:followed_users)}
+	it { should respond_to(:following?) }
+	it { should respond_to(:follow!)}
+	it { should respond_to(:unfollow!)}
+	it { should respond_to(:reverse_relationships)}
+	it { should respond_to(:followers)}
 
 	it {should be_valid}
 	it {should_not be_admin}
+
+	describe "following" do
+		let(:other_user) {FactoryGirl.create(:user)}
+		before do
+			@user.save
+			@user.follow!(other_user)
+		end
+
+		it { should be_following(other_user)}
+		its(:followed_users) { should include(other_user)}
+
+	    describe "and unfollowing" do
+	      before { @user.unfollow!(other_user) }
+
+	      it { should_not be_following(other_user) }
+	      its(:followed_users) { should_not include(other_user) }
+	    end
+
+		describe "followed user" do
+			subject { other_user }
+			its(:followers) { should include(@user) }
+		end
+	end
+
 	describe "with admin attribute set to 'true'" do
 		before do
 			@user.save!
@@ -137,10 +172,30 @@ describe User do
 		let!(:newer_micropost) do
 			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
 		end
+
+		describe "status" do
+			let(:unfollowed_post) do
+				FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+			end
+			let(:followed_user) {FactoryGirl.create(:user)}
+			before do
+				@user.follow!(followed_user)
+				3.times { followed_user.microposts.create!(content: "Lorem ipsum")}
+			end
+
+			its(:feed) { should include(newer_micropost)}
+			its(:feed) { should include(older_micropost)}
+			its(:feed) { should_not include(unfollowed_post)}
+			its(:feed) do
+				followed_user.microposts.each do |micropost|
+					should include(micropost)
+				end
+			end
+		end
+
 		it "should have the right microposts in the right order" do
 			@user.microposts.should == [newer_micropost, older_micropost]
 		end
-
 
 		it "should destroy associated microposts" do
 			microposts = @user.microposts.dup
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,11 +10,38 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
   end
 
-describe "Home page" do
+  describe "Home page" do
     before { visit root_path }
     let(:heading) {'Sample App'}
     let(:page_title) {''}
     let(:heading_) {'| Home'}
+    describe "for signed-in users" do
+      let(:user) {FactoryGirl.create(:user)}
+
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) {FactoryGirl.create(:user)}
+        before do
+          other_user.follow!(user)
+          visit root_path 
+        end
+
+        it { should have_link("0 following", href: following_user_path(user))}
+        it { should have_link("1 followers", href: followers_user_path(user))}
+      end
+    end
   end
 
   describe "Help page" do
@@ -45,5 +72,5 @@ describe "Home page" do
     page.should have_selector 'title', text: full_title('Contact')
     click_link "sample app"
     page.should have_selector 'title', text: "Ruby on Rails Tutorial Sample App"
-  end  
+  end
 end
